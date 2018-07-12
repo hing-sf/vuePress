796fc340ac87dfe4b5596c1adc46f9b66b8d5c40
Hing Tang <htang@levi.com>
2018-07-12 01:06:55 -0700

    first commit

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        vueReact
+      </h1>
+      get your json here!!
+    </div>
+  );
+};
+
+export default Landing;
---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,5 @@
+export default [
+  { label: 'json Data', name: 'jsonData' },
+  { label: 'Module Type', name: 'type' }
+  // { label: 'Recipient List', name: 'recipients' }
+];
